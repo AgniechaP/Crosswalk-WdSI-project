8e801ec578df573e9d95187617f938505b48a7c6
Agnieszka_Piorko <agnpior@gmail.com>
2022-02-16 14:31:23 +0100

    First steps - copy from another repo

---
+++ csv_Test.py
@@ -0,0 +1,48 @@
+# Importowanie potrzebnych bibliotek:
+import xml.etree.ElementTree as Xet
+import pandas as pd
+import os
+import glob2
+
+cols = ["Width", "Height", "Roi.X1", "Roi.Y1", "Roi.X2", "Roi.Y2", "ClassId", "Path"]
+rows = []
+Class_from_xml = 0
+how_many_1 = 0
+how_many_0 = 0
+how_many_1_Test = 0
+how_many_0_Test = 0
+# Zliczanie liczby plikow w folderze Test_xml:
+path, dirs, files = next(os.walk("./Test_xml"))
+file_count = len(files)
+
+#for j in range(0, file_count):
+for filename in glob2.glob(os.path.join(path, '*.xml')):
+   with open(os.path.join(os.getcwd(), filename), 'r') as f:
+    xmlparse = Xet.parse(f)
+    root = xmlparse.getroot()
+    root2 = xmlparse.findall("object")
+    Path = xmlparse.find("filename").text
+    Width = xmlparse.find("size/width").text
+    Height = xmlparse.find("size/height").text
+    for i in root2:
+        if i:
+            RoiX1 = i.find("bndbox/xmin").text
+            RoiY1 = i.find("bndbox/ymin").text
+            RoiX2 = i.find("bndbox/xmax").text
+            RoiY2 = i.find("bndbox/ymax").text
+            Class_from_xml = i.find("name").text
+            if Class_from_xml == 'crosswalk':
+                ClassId = 1
+                how_many_1 = how_many_1+1
+            else:
+                ClassId = 0
+                how_many_0=how_many_0+1
+
+        rows.append([Width, Height, RoiX1, RoiY1, RoiX2, RoiY2, ClassId, 'Test/' + Path])
+    df = pd.DataFrame(rows, columns=cols)
+
+    # Tworzenie pliku .csv i zapis do niego:
+    df.to_csv('Test.csv')
+print("Liczba plikow w Test_xml:", file_count)
+print("Liczba znakow crosswalks Test:", how_many_1)
+print("Liczba znakow innych niz crosswalks Test:", how_many_0)
---
+++ main.py
@@ -0,0 +1,83 @@
+import os
+import random
+import numpy as np
+import cv2
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix
+import pandas
+
+def load_data(path, filename):
+    """
+    Loads data from disk.
+    @param path: Path to dataset directory.
+    @param filename: Filename of csv file with information about samples.
+    @return: List of dictionaries, one for every sample, with entries "image" (np.array with image) and "label" (class_id).
+    """
+    entry_list = pandas.read_csv(os.path.join(path, filename))
+
+    data = []
+    for idx, entry in entry_list.iterrows():
+        class_id = entry['ClassId']
+        image_path = entry['Path']
+
+        if class_id != -1:
+            image = cv2.imread(os.path.join(path, image_path))
+            data.append({'image': image, 'label': class_id})
+
+    return data
+
+def display_dataset_stats(data):
+    """
+    Displays statistics about dataset in a form: class_id: number_of_samples
+    @param data: List of dictionaries, one for every sample, with entry "label" (class_id).
+    @return: Nothing
+    """
+    class_to_num = {}
+    for idx, sample in enumerate(data):
+        class_id = sample['label']
+        if class_id not in class_to_num:
+            class_to_num[class_id] = 0
+        class_to_num[class_id] += 1
+
+    class_to_num = dict(sorted(class_to_num.items(), key=lambda item: item[0]))
+    # print('number of samples for each class:')
+    print(class_to_num)
+
+def learn_bovw(data):
+    """
+    Learns BoVW dictionary and saves it as "voc.npy" file.
+    @param data: List of dictionaries, one for every sample, with entries "image" (np.array with image) and "label" (class_id).
+    @return: Nothing
+    """
+    dict_size = 128
+    bow = cv2.BOWKMeansTrainer(dict_size)
+
+    sift = cv2.SIFT_create()
+    for sample in data:
+        kpts = sift.detect(sample['image'], None)
+        kpts, desc = sift.compute(sample['image'], kpts)
+
+        if desc is not None:
+            bow.add(desc)
+
+    vocabulary = bow.cluster()
+
+    np.save('voc.npy', vocabulary)
+
+def main():
+    data_train = load_data('./', 'Train.csv')
+    print('train dataset before balancing:')
+    display_dataset_stats(data_train)
+
+    data_test = load_data('./', 'Test.csv')
+    print('test dataset before balancing:')
+    display_dataset_stats(data_test)
+
+    # you can comment those lines after dictionary is learned and saved to disk.
+    print('learning BoVW')
+    learn_bovw(data_train)
+
+
+
+if __name__ == '__main__':
+    main()
